48602a22248e405bd734d13ee2f100fed48c491d
yakov <mirenkovye@yandex.ru>
2020-05-27 15:14:32 +0300

    New task

--- Process.c
+++ Process.c
@@ -2,8 +2,8 @@
 
 void Process(double *matr, int N, int M)
 {
-	int i, j, i_max = 0, j_max = 0;
-	double *b, max = matr[0], sum = 0;
+	int i, j, i_max = 0;
+	double *b, *c, max = matr[0], sum;
 
 	for( i = 0, b = matr; i<N; i++, b += M )
 		for( j = 0; j<M; j++ )
@@ -11,13 +11,14 @@ void Process(double *matr, int N, int M)
 			{
 				max = b[j];
 				i_max = i;
-				j_max = j;
 			}
 
-	for( i = 0, b = matr + j_max; i<N; i++, b += M )
-		sum += *b;
-	sum -= max;
-
 	for( j = 0, b = matr + i_max*M; j<M; j++ )
-		b[j] = sum;
+	{
+		sum = 0;
+		for( i = 0, c = matr + j; i<N; i++, c += M )
+			sum += *c;
+
+		b[j] = sum - b[j];
+	}
 }
